aaf7cf45698a45216c7364bfaa5bd6b09460e63a
Daniel Fernandes <danielsf97@gmail.com>
2020-06-20 09:49:50 +0100

    Removed Deploy files

--- CLOUD/Deploy/lsfs_deploy_ansible/script.sh
+++
@@ -1,1 +0,0 @@
-step=`bc -l <<< 'scale=6; 1/100'`;res=`seq 0 ${step} 1`;exit $res
--- CLOUD/Deploy/lsfs_deploy_ansible/test.py
+++
@@ -1,8 +0,0 @@
-try:
-    import kubernetes
-    import openshift
-    from openshift.dynamic import DynamicClient
-    from openshift.dynamic.exceptions import ResourceNotFoundError, ResourceNotUniqueError
-    print "Found it"
-except ImportError:
-    print "Didnt find it"
