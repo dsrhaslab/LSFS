736bb4704b7dd6bed04ef1003f42de9b291af8ab
Daniel Fernandes <danielsf97@gmail.com>
2020-08-21 23:21:53 +0100

    view recovering in smart load balancer

--- df_loadbalancing/smart_load_balancer.h
+++ df_loadbalancing/smart_load_balancer.h
@@ -27,7 +27,7 @@ private:
     int replication_factor_max;
     int replication_factor_min;
     int max_age;
-    std::atomic<bool> first_message; //se estamos a iniciar o protocolo de group construction
+    std::atomic<bool> recovering_local_view;
 
     std::mt19937 random_eng;
     std::atomic<bool> running;
@@ -38,10 +38,19 @@ private:
 
 private:
     peer_data get_random_local_peer();
+    void receive_local_message(std::vector<peer_data> received);
     void receive_message(std::vector<peer_data> received);
     int group(double peer_pos);
     void merge_groups_from_view();
     void split_groups_from_view();
+    void incorporate_local_peers(const std::vector<peer_data>& received);
+    void clean_local_view();
+    void incorporate_peers_in_view(const std::vector<peer_data>& received);
+    void reconfigure_view_if_needed();
+    void recover_local_view(const std::vector<peer_data>& received);
+    bool has_recovered();
+    void request_local_message(const std::vector<peer_data>& received);
+    void send_msg(const peer_data& target_peer, proto::pss_message& msg);
 
 public:
     smart_load_balancer(std::string boot_ip, std::string ip, long sleep_interval, std::string& config_filename);
@@ -49,14 +58,10 @@ public:
     peer_data get_random_peer() override;
     std::vector<peer_data> get_n_peers(const std::string& key, int nr_peers) override;
     std::vector<peer_data> get_n_random_peers(int nr_peers) override;
-    void receive_local_message(std::vector<peer_data> received);
     void process_msg(proto::pss_message& msg) override;
     void operator()() override;
 
     void stop() override;
-
-private:
-    void send_msg(peer_data& target_peer, proto::pss_message& msg);
 };
 
 
